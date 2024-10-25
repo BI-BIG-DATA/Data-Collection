ba4d92292ab62437432bb56103e9d64510405766
samir5636 <zianisamir5636789@gmail.com>
2024-10-26 00:48:06 +0100

    v1

--- iee/iee/settings.py
+++ iee/iee/settings.py
@@ -114,3 +114,8 @@ MONGO_URI = 'mongodb://localhost:27017'
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+# FEED_FORMAT = "json"
+# FEED_URI = "output.json"
+# settings.py
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
